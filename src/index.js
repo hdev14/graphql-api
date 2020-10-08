f1c48f25a2cb410fa8a87f6c8ddd348dd964d6b0
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-08 13:11:15 -0300

    feat: add graphqlHTTP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const expressGraphql = require('express-graphql')
 
 const server = express()
+
+server.use('/graphql', expressGraphql.graphqlHTTP({
+  graphiql: true,
+}))
 
 server.listen(4000, () => console.log('Server is running'))
